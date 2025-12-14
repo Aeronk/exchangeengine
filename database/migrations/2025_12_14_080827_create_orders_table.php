77bb55fd4f3d1515231c2dfdee89f55de9666140
Aaron Katema <aaronkatema@gmail.com>
2025-12-14 10:17:08 +0200

    feat: add users balance, assets, orders, trades with optimized indexes

---
+++ database/migrations/2025_12_14_080827_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('symbol', 10);
+            $table->enum('side', ['buy', 'sell']);
+            $table->decimal('price', 20, 8);
+            $table->decimal('amount', 20, 8);
+            $table->tinyInteger('status')->default(1)->comment('1=open, 2=filled, 3=cancelled');
+            $table->decimal('locked_value', 20, 8)->default(0)->comment('USD for buy, Asset amount for sell');
+            $table->timestamps();
+            
+            $table->index(['symbol', 'status', 'side', 'price']);
+            $table->index(['user_id', 'status']);
+            $table->index('created_at');
+     
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
